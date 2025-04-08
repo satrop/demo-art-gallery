c2861622882eab169e15ad41b608267cf72e43ac
Steven Portas <stevenportas@me.com>
2025-04-08 14:08:38 -0700

    feat: initialize Astro project with basic structure and assets
    - Added package.json for project dependencies and scripts.
    - Included favicon.svg for the site's favicon.
    - Added astro.svg and background.svg assets for branding and background.
    - Created Welcome component to serve as the landing page.
    - Implemented Layout component for consistent HTML structure.
    - Set up index.astro as the main entry point, rendering the Welcome component.
    - Configured TypeScript with tsconfig.json for strict type checking.

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://satrop.github.io",
+  base: "/demo-art-gallery",
+});
